c2f8b50ce9e35a18441daa7f1ff568cfe65784a3
geovanirangel <dev.geovanirangel@gmail.com>
2020-03-03 14:52:44 -0300

    Update Config.php

--- tests/Config.php
+++ tests/Config.php
@@ -6,7 +6,7 @@ define("DBCONFIG", [
     "host" => "localhost",
     "port" => "3306",
     "charset" => "utf8",
-    "dbname" => "dname",
+    "dbname" => "dbname",
     "users" => [
         "default" => [
             "name" => "root",
